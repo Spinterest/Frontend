ae4abc530a39426dce2663eed7efb1d53843a1f2
Abdul Gany Osman <Abdul.Osman@bbd.co.za>
2024-05-03 13:26:31 +0200

    Removed placeholder text

--- src/components/board-feed/board-feed.js
+++ src/components/board-feed/board-feed.js
@@ -23,7 +23,7 @@ export class BoardFeed extends HTMLElement {
     }
 
     populateFeed() {
-        customElements.define('board-card', BoardCard);
+        
     }
 
     addButtonEvents() {
--- src/script.js
+++ src/script.js
@@ -2,10 +2,12 @@ import {Router} from "./js/Router.js";
 import {NavigationBar} from "./components/navigation-bar/navigation-bar.js";
 import {MasonryFeed} from "./components/masonry-feed/masonry-feed.js";
 import {BoardFeed} from "./components/board-feed/board-feed.js";
+import {BoardCard} from "./components/board-card/board-card.js";
 
 
 customElements.define('navigation-bar', NavigationBar);
 customElements.define('masonry-feed', MasonryFeed);
 customElements.define('board-feed', BoardFeed);
+customElements.define('board-card', BoardCard);
 
 const router = new Router();
