a059e9c2ae3112b3aa379c2c203a642dae64e292
Kat <Katlego.Kungoane@bbd.co.za>
2024-05-08 15:38:48 +0200

    some functionality for tags

--- src/components/masonry-feed/masonry-feed.js
+++ src/components/masonry-feed/masonry-feed.js
@@ -1,3 +1,25 @@
+/*
+TODO
+TAGS DONT OVERFLOW NICELY
+THEY EXTENT THE CREATE SPIN WINDOW
+asdsaasd
+sa
+d
+as
+
+d
+as
+d
+sa
+
+d
+asdasda
+todo
+todo
+todo
+ */
+
+
 import {Router} from "../../js/Router.js";
 import {Toast} from "../../js/Toast.js";
 
@@ -12,8 +34,8 @@ export class MasonryFeed extends HTMLElement {
     constructor() {
         super();
 
-        this.upload = this.hasAttribute('upload');
-        this.webView = this.hasAttribute('webView');
+        this.upload = this.hasAttribute('upload') && !this.hasAttribute('webView');
+        this.webView = this.hasAttribute('webView') && !this.hasAttribute('upload');
 
         this.complexClass = new ComplexController ();
         this.spinClass = new SpinController();
@@ -98,6 +120,10 @@ export class MasonryFeed extends HTMLElement {
 
     // TODO: Infinity Scroll
     populateFeed(data) {
+        if (!data || !Array.isArray(data)) {
+            data = [data];
+        }
+
         const article = this.querySelector('article');
 
         if (this.upload) {
@@ -138,13 +164,63 @@ export class MasonryFeed extends HTMLElement {
             buttonCreate.addEventListener("click", () => 
             {
                 this.closeModal(modal);
-            })
+            });
+
+            const tagsInput = document.getElementById("inpTag");
+            const tagOverFlow = document.getElementById("tag-overflow-area");
+
+            const addTagButton = document.getElementById("add-tag-button");
+            addTagButton.addEventListener("click",
+                (event) =>{
+                    event.preventDefault();
+
+                    if (tagsInput.value.length === 0){
+                        new Toast(`Enter text to add tag`, 'error');
+                        return
+                    }
+
+                    const existingTagOverflowSection = document.getElementById(`tag-overflow-section-${tagsInput.value.toLowerCase()}`);
+                    if (!existingTagOverflowSection){
+                        const tagOverflowSection = document.createElement("section");
+                        tagOverflowSection.id = `tag-overflow-section-${tagsInput.value.toLowerCase()}`;
+                        tagOverflowSection.classList.add("overlay-wrapper");
+                        tagOverflowSection.classList.add("overlay-wrapper-tag-button");
+
+                        const tagButton = document.createElement("button");
+                        tagButton.classList.add("tag-button");
+                        tagButton.textContent = tagsInput.value.toLowerCase();
+                        tagsInput.value = '';
+                        tagButton.id = 'tag-overflow-section-button';
+
+                        const tagButtonDelete = document.createElement("button");
+                        tagButtonDelete.classList.add("tag-button-delete");
+                        tagButtonDelete.textContent = 'X';
+
+                        tagOverflowSection.appendChild(tagButton);
+                        tagOverflowSection.appendChild(tagButtonDelete);
+                        tagOverFlow.appendChild(tagOverflowSection);
+
+                        tagButtonDelete.addEventListener('click', (event) => {
+                            console.log("clicked");
+                            tagOverFlow.removeChild(tagOverflowSection);
+                        });
+
+                        tagButton.addEventListener('click', (event) =>{
+                            console.log("clocked but button");
+                        })
+                    }
+                    else {
+                        const tagButton = existingTagOverflowSection.querySelector("#tag-overflow-section-button");
+                        new Toast(`You have already added tag: ${tagButton.textContent}`, 'info');
+                    }
+                }
+            );
         }
 
         const router = new Router();
         data.forEach(spin => {
             const imageSection = document.createElement('section');
-            imageSection.setAttribute('class', 'image-wrapper');
+            imageSection.setAttribute('class', 'overlay-wrapper');
 
             const dropDownContainer = document.createElement('div');
             dropDownContainer.setAttribute('class', 'dropdown');
@@ -183,7 +259,7 @@ export class MasonryFeed extends HTMLElement {
             }
 
             const imageButton = document.createElement('button');
-            imageButton.setAttribute('class', 'image-button');
+            imageButton.setAttribute('class', 'overlay-button');
             imageButton.classList.add('hidden');
             imageButton.textContent = "X";
 
@@ -210,7 +286,7 @@ export class MasonryFeed extends HTMLElement {
             imageSection.appendChild(dropDownContainer);
 
             article.appendChild(imageSection);
-        })
+        });
     }
 
     closeModal(modal) {
@@ -233,6 +309,4 @@ export class MasonryFeed extends HTMLElement {
 
         modal.close();
     }
-
-
 }
